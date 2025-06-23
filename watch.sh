e5b01ee64d0034fe17c51605a84d0885aa9838ea
LiamKearn <contact@liamkearn.me>
2025-06-23 18:31:06 +1000

    Also watch new file

--- watch.sh
+++ watch.sh
@@ -4,7 +4,9 @@ LAST=""
 while true; do
   H_HASH=$(md5 -q docs/index.html)
   J_HASH=$(md5 -q docs/index.js)
-  [ "$H_HASH$J_HASH" != "$LAST" ] && echo "$H_HASH$J_HASH" > docs/reload && LAST="$H_HASH$J_HASH"
+  T_HASH=$(md5 -q docs/trackviz.js)
+  ALL_HASH="$H_HASH$J_HASH$T_HASH"
+  [ "$ALL_HASH" != "$LAST" ] && echo "$ALL_HASH" > docs/reload && LAST="$ALL_HASH"
   sleep 1
 done
 
